f1ca59c26a7060585605292b255adb8de2262391
shanto <hhassibul@gmail.com>
2022-06-07 16:54:05 +0600

    awkueytrpu

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-var arr = [3, 7, 8, 88, 98]
+var arr = [3, 7, 8, 88, 102, 2]
 
 // for (var i = 0; i < arr.length; i++){
 //     arr[i] +=  2
@@ -7,6 +7,8 @@ var arr = [3, 7, 8, 88, 98]
 //   console.log(arr);
 var sum = 0
 for (var i = 0; i < arr.length; i++){
-  sum += arr[i];
+if(arr[i] % 2 == 1){
+  sum += arr[i] 
+}  
 }
 console.log(sum);
