3c5c4175abaecb6d566cc702cf567026083bb03a
shanto <hhassibul@gmail.com>
2022-06-10 19:23:43 +0600

    skd

---
+++ arrayFunction.js
@@ -0,0 +1,17 @@
+var array1 =[1,2,3]
+var array2 =[5,2,3]
+var array3 =[1,7,3,7]
+var resust = 0
+
+
+function sum (a){
+  var r = 0;
+  for (let i = 0; i<a.length; i++){
+    r += a[i]
+    
+    }
+    console.log(r);
+}
+sum(array1)
+sum(array2)
+sum(array3)
