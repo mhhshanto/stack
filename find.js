ed1dbe05ef8ceadb3b99b87d6751a44b2e6961e9
shanto <hhassibul@gmail.com>
2022-06-12 21:22:22 +0600

    hg

---
+++ find.js
@@ -0,0 +1,5 @@
+var arr = [1,3,4,5,6,8,84,48]
+var result = arr.find(function(value){return value === 3})
+console.log(result);
+var resultIndex = arr.findIndex(function(value){return value === 48})
+console.log(resultIndex);
