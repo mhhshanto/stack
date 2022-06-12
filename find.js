28ef0b23370aee918675b7541b6a24785680090c
shanto <hhassibul@gmail.com>
2022-06-13 01:04:44 +0600

    sdku

--- find.js
+++ find.js
@@ -3,3 +3,4 @@ var result = arr.find(function(value){return value === 3})
 console.log(result);
 var resultIndex = arr.findIndex(function(value){return value === 48})
 console.log(resultIndex);
+// shanto
